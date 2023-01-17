6ee430f5278472a0b4d3ba4a92f3fe593ffca649
Mehedi Hasan Munna <mehedihasanmunna516@gmail.com>
2023-01-17 22:23:43 +0600

    1st

---
+++ 1. C & C++ Concept/10. Structure & Function/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+struct rectangle
+{
+    int length;
+    int breadth;
+};
+void initialize(struct rectangle *r, int l, int b)
+{
+    r->length = l;
+    r->breadth = b;
+}
+int area(struct rectangle x)
+{
+    return x.length * x.breadth;
+}
+void change_value(struct rectangle *p, int l)
+{
+    p->length = l;
+}
+int main()
+{
+    struct rectangle r;
+    initialize(&r, 10, 5);
+    int y = area(r);
+    change_value(&r, 20);
+    cout << "Length  : " << r.length << " "
+         << " Breadth : " << r.breadth << endl;
+
+    return 0;
+}
---
+++ 1. C & C++ Concept/11. Class In C++/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+class rectangle
+{
+private:
+    int length;
+    int breadth;
+
+public:
+    rectangle()
+    {
+        length = 0;
+        breadth = 0;
+    }
+
+    rectangle(int l, int b)
+    {
+        length = l;
+        breadth = b;
+    }
+    int area()
+    {
+        return length * breadth;
+    }
+    int change_value(int l)
+    {
+        return length = l;
+    }
+};
+int main()
+{
+    rectangle r(10, 5);
+    cout << r.area() << endl;
+    cout << r.change_value(60) << endl;
+    return 0;
+}
