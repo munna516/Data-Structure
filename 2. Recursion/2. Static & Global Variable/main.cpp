ceb468e0f45bc28dc157abe9f63088b2075f1d63
Mehedi Hasan Munna <mehedihasanmunna516@gmail.com>
2023-01-24 10:23:20 +0600

    2nd

---
+++ 2. Recursion/2. Static & Global Variable/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int fun(int a)
+{
+    if (a > 0)
+    {
+        return fun(a - 1) + a;
+    }
+    return 0;
+}
+int main()
+{
+    int r = 5;
+    cout << fun(r) << endl;
+
+    return 0;
+}
---
+++ 2. Recursion/2. Static & Global Variable/main2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int fun(int a)
+{
+    static int x = 0;
+    if (a > 0)
+    {
+        ++x;
+        return fun(a - 1) + x;
+    }
+    return 0;
+}
+int main()
+{
+    int r = 5;
+    cout << fun(r) << endl;
+
+    return 0;
+}
