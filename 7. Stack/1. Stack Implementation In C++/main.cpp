02b39bdd55604947625209694772a9156b7f197e
Mehedi Hasan Munna <mehedihasanmunna516@gmail.com>
2023-04-04 16:51:09 +0600

    7th

--- 7. Stack/1. Stack Implementation In C++/main.cpp
+++ 7. Stack/1. Stack Implementation In C++/main.cpp
@@ -46,10 +46,69 @@ public:
         return A[stack_size - 1];
     }
 };
+
+// Stack Implementation Using Dynamic Array
+class Stack_Dynamic_Array
+{
+public:
+    int *A;
+    int array_cap;
+    int stack_size;
+    Stack_Dynamic_Array()
+    {
+        A = new int[1];
+        array_cap = 1;
+        stack_size = 0;
+    }
+
+    // Increasing Array
+    void Increase_Array()
+    {
+        int *temp;
+        temp = new int[array_cap * 2];
+        for (int i = 0; i < array_cap; i++)
+            temp[i] = A[i];
+        swap(A, temp);
+        delete[] temp;
+        array_cap = array_cap * 2;
+    }
+
+    // Adding Element At Stack
+    void push(int value)
+    {
+        if (stack_size + 1 > array_cap)
+        {
+            Increase_Array();
+        }
+        stack_size++;
+        A[stack_size - 1] = value;
+    }
+
+    // Deleting Element At a Stack
+    void pop()
+    {
+        if (stack_size == 0)
+        {
+            cout << "Stack is Empty!" << endl;
+            return;
+        }
+        A[stack_size - 1] = 0;
+        stack_size--;
+    }
+
+    // Top Element
+    int Top()
+    {
+        if (stack_size == 0)
+            return -1;
+        return A[stack_size - 1];
+    }
+};
+
 int main()
 {
-    Stack_Static_Array st;
-    // st.push(10);
+    Stack_Dynamic_Array st;
+    st.push(100);
     cout << st.Top() << endl;
     st.push(20);
     st.push(30);
