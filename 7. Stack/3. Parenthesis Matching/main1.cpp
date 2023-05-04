9c31ee3c2c26d30375e7eca28c38dc4ebdc606b5
Mehedi Hasan Munna <mehedihasanmunna516@gmail.com>
2023-05-04 11:55:31 +0600

    7th

---
+++ 7. Stack/3. Parenthesis Matching/main1.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Stack
+{
+public:
+    int size;
+    int top;
+    char *s;
+    Stack()
+    {
+        size = 0;
+    }
+
+    // Pushing element
+    void push(char value)
+    {
+        if (top == size - 1)
+            cout << "Stack is full" << endl;
+        else
+        {
+            top++;
+            s[top] = value;
+        }
+    }
+
+    // Pop Element
+    int pop()
+    {
+        int x = -1;
+        if (top == -1)
+            cout << "Stack is Empty!!" << endl;
+        else
+        {
+            x = s[top];
+            top--;
+        }
+        return x;
+    }
+
+    // Top Element
+    int top_element()
+    {
+        if (top == -1)
+            return -1;
+        else
+            return s[top];
+    }
+
+    // stack is empty
+    int isEmpty()
+    {
+        if (top == -1)
+            return 1;
+        else
+            return 0;
+    }
+    // Stack is Full
+    int isFull()
+    {
+        if (top == size - 1)
+            return 1;
+        else
+            return 0;
+    }
+
+    // Parenthesis Matching
+    int isBalanced(char *exp)
+    {
+        size = strlen(exp);
+        top = -1;
+        s = new char[size];
+        for (int i = 0; exp[i] != '\0'; i++)
+        {
+            if (exp[i] == '(')
+                push(exp[i]);
+            else if (exp[i] == ')')
+            {
+                if (isEmpty())
+                    return 0;
+                else
+                    pop();
+            }
+        }
+        if (isEmpty())
+            return 1;
+        else
+            return 0;
+    }
+};
+
+int main()
+{
+    Stack s;
+    char *exp = "(((a+b*c)-(b-d+c)))";
+    cout << s.isBalanced(exp) << endl;
+
+    return 0;
+}
