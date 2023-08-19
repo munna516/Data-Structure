00035779fc9c03ce926aa15d56ea94c7d0cb1cef
Mehedi Hasan Munna <mehedihasanmunna516@gmail.com>
2023-08-19 21:23:26 +0600

    Update

---
+++ 9. Trees/main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Queue
+{
+    int size;
+    int front;
+    int rear;
+    int *Q;
+};
+
+// Creating Queue
+void CreateQueue(struct Queue *q, int size)
+{
+    q->size = size;
+    q->Q = (int *)malloc(q->size * sizeof(int));
+    q->front = -1;
+    q->rear = -1;
+}
+
+// Display Queue
+void display(struct Queue q)
+{
+    for (int i = q.front + 1; i <= q.rear; i++)
+        printf("%d ", q.Q[i]);
+    printf("\n");
+}
+// Inserting in queue
+void enqueue(struct Queue *q, int x)
+{
+    if (q->rear == q->size - 1)
+        printf("Queue is Full\n");
+    else
+    {
+        q->rear++;
+        q->Q[q->rear] = x;
+    }
+}
+
+// Delete from queue
+int dequeue(struct Queue *q)
+{
+    int x = -1;
+    if (q->rear == q->front)
+        printf("Queue is Empty!!\n");
+    else
+    {
+        q->front++;
+        x = q->Q[q->front];
+    }
+    return x;
+}
+int main()
+{
+}
