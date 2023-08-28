e8d00d8a45ba23d18c9f810bab18141efd6e9dd7
Mehedi Hasan Munna <mehedihasanmunna516@gmail.com>
2023-08-28 19:30:44 +0600

    Update

---
+++ 9. Trees/Array Representation/main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+char ary[100];
+void root(char key)
+{
+    if (ary[0] != '\0')
+        cout << "Tree Had Already Root" << endl;
+    else
+        ary[0] = key;
+}
+int main()
+{
+}
